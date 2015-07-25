76b0d70e4ccb1caa6e5b328abc17c003354ed8d0
clark.kang <clark.kang@kakao.com>
2015-07-25 23:45:16 +0900

    support nested multibulk reply
    fix bug for parsing nested multibulk reply

--- src/nc_message.h
+++ src/nc_message.h
@@ -34,6 +34,8 @@ typedef enum msg_parse_result {
     MSG_PARSE_AGAIN,                      /* incomplete -> parse again */
 } msg_parse_result_t;
 
+#define NC_MULTIBULK_DEPTH               3
+
 #define MSG_TYPE_CODEC(ACTION)                                                                      \
     ACTION( UNKNOWN )                                                                               \
     ACTION( REQ_MC_GET )                       /* memcache retrieval requests */                    \
@@ -215,6 +217,7 @@ struct msg {
     uint32_t             mlen;            /* message length */
     int64_t              start_ts;        /* request start timestamp in usec */
 
+    int                  depth;           /* current nested multibulk depth */
     int                  state;           /* current parser state */
     uint8_t              *pos;            /* parser position marker */
     uint8_t              *token;          /* token marker */
@@ -241,6 +244,7 @@ struct msg {
     uint8_t              *narg_end;       /* narg end (redis) */
     uint32_t             narg;            /* # arguments (redis) */
     uint32_t             rnarg;           /* running # arg used by parsing fsa (redis) */
+    uint32_t             rnargs[NC_MULTIBULK_DEPTH];
     uint32_t             rlen;            /* running length in parsing fsa (redis) */
     uint32_t             integer;         /* integer reply value (redis) */
 
